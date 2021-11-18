394a10031307e9e8e8679b22a2db86ee4b663ecd
Abhilash <abhilashgraja@gmail.com>
2021-11-18 10:40:52 +0530

    fixes for exception handler path & hide actions during add/edit dialog

--- Step4/ASPSecurityKitConfiguration.cs
+++ Step4/ASPSecurityKitConfiguration.cs
@@ -58,7 +58,7 @@ namespace SuperCRM
 			}
 			else
 			{
-				app.UseExceptionHandler();
+				app.UseExceptionHandler("/Home/Error");
 
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
